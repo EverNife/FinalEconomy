b555bcc8e8735a7e75e3b05a5fda52ac0ad2a062
Petrus <petrus_ant@hotmail.com>
2022-02-07 19:51:32 -0300

    Create way to show all BallTop

--- src/main/java/br/com/finalcraft/finaleconomy/PermissionNodes.java
+++ src/main/java/br/com/finalcraft/finaleconomy/PermissionNodes.java
@@ -5,6 +5,7 @@ public class PermissionNodes {
     public static final String COMMAND_BALANCE          = "finaleconomy.command.balance";
     public static final String COMMAND_BALANCE_OTHER    = "finaleconomy.command.balance.other";
     public static final String COMMAND_BALANCETOP       = "finaleconomy.command.balancetop";
+    public static final String COMMAND_BALANCETOP_ALL   = "finaleconomy.command.balancetop.all";
     public static final String COMMAND_PAY              = "finaleconomy.command.pay";
     public static final String COMMAND_ECO              = "finaleconomy.command.eco";
 
--- src/main/java/br/com/finalcraft/finaleconomy/commands/CMDBalanceTop.java
+++ src/main/java/br/com/finalcraft/finaleconomy/commands/CMDBalanceTop.java
@@ -3,9 +3,9 @@ package br.com.finalcraft.finaleconomy.commands;
 import br.com.finalcraft.evernifecore.argumento.MultiArgumentos;
 import br.com.finalcraft.evernifecore.commands.finalcmd.annotations.FinalCMD;
 import br.com.finalcraft.evernifecore.config.playerdata.PlayerController;
+import br.com.finalcraft.evernifecore.util.FCBukkitUtil;
 import br.com.finalcraft.evernifecore.util.FCTextUtil;
-import br.com.finalcraft.evernifecore.util.numberwrapper.NumberWrapper;
-import br.com.finalcraft.evernifecore.util.pageviwer.PageViwer;
+import br.com.finalcraft.evernifecore.util.pageviwer.PageViewer;
 import br.com.finalcraft.finaleconomy.PermissionNodes;
 import br.com.finalcraft.finaleconomy.config.data.FEPlayerData;
 import org.bukkit.command.CommandSender;
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 
 public class CMDBalanceTop {
 
-    private final PageViwer<FEPlayerData, Double> BAL_TOP = PageViwer.builder(
+    private final PageViewer<FEPlayerData, Double> BAL_TOP = PageViewer.builder(
             () -> PlayerController.getAllPlayerData(FEPlayerData.class).stream().collect(Collectors.toList()),
             playerData -> playerData.getMoney())
             .setFormatHeader(
@@ -26,20 +26,29 @@ public class CMDBalanceTop {
             .addPlaceholder("%money_formatted%", FEPlayerData::getMoneyFormatted)
             .setIncludeDate(true)
             .setIncludeTotalPlayers(true)
-            .setLineEnd(100)
+            .setLineEnd(-1)
             .build();
 
     @FinalCMD(
             aliases = {"balancetop","baltop","moneytop","febalancetop"},
-            usage = "<Player>",
+            usage = "[-all]",
             permission = PermissionNodes.COMMAND_BALANCETOP
     )
     public void top(CommandSender sender, MultiArgumentos argumentos){
-        NumberWrapper<Integer> page = argumentos.get(0)
-                .getNumberWrapper(Integer.class, 1)
-                .bound(1, 10);
 
-        BAL_TOP.send(page.get(), sender);
+        if (argumentos.get(0).equalsIgnoreCase("-all")){
+            if (!FCBukkitUtil.hasThePermission(sender, PermissionNodes.COMMAND_BALANCETOP_ALL)){
+                return;
+            }
+            BAL_TOP.send(BAL_TOP.getLineStart(), BAL_TOP.getLineEnd(), sender);
+            return;
+        }
+
+        int page = argumentos.get(0).getNumberWrapper(Integer.class, 1)
+                .boundLower(1) //0 page to Infinity
+                .intValue();
+
+        BAL_TOP.send(page, sender);
     }
 
 }
