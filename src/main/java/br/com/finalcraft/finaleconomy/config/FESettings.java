e693dbcd1d948204d7f47be92745895436b4c416
Pétrus Pradella <petrus_ant@hotmail.com>
2022-05-26 09:07:37 -0300

    Add comment for 'Settings.moneyFormatLocale'

--- src/main/java/br/com/finalcraft/finaleconomy/config/FESettings.java
+++ src/main/java/br/com/finalcraft/finaleconomy/config/FESettings.java
@@ -10,7 +10,14 @@ public class FESettings {
     public static NumberFormat MONEY_FORMATTER = NumberFormat.getNumberInstance(Locale.forLanguageTag("pt_BR"));
 
     public static void initialize(){
-        String localeMoneyFormatter = ConfigManager.getMainConfig().getOrSetDefaultValue("Settings.moneyFormatLocale", "en_US");
+
+        String localeMoneyFormatter = ConfigManager.getMainConfig().getOrSetDefaultValue(
+                "Settings.moneyFormatLocale",
+                "en_US",
+                "This means the way the number will be formatted, for example: " +
+                        "\non 'en_US' the value of 10000000 will be 100,000,000" +
+                        "\non 'pt_BR' the value of 10000000 will be 100.000.000"
+        );
 
         Locale locale = Locale.forLanguageTag(localeMoneyFormatter);
         if (locale == null){
